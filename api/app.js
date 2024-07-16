675f263a4d99c3fe1551193c400fcb4071a3a690
tjsiv <tjsivelsjr@gmail.com>
2024-07-15 19:22:39 -0700

    test

--- api/app.js
+++ api/app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 //cors bypass
 var cors = require("cors");
 
-var indexRouter = require('./routes/index');
+var recipeRouter = require('./routes/recipeLinks');
 var usersRouter = require('./routes/users');
 var testAPIRouter = require("./routes/testAPI");
 var app = express();
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', recipeRouter);
 app.use('/users', usersRouter);
 app.use("/testAPI", testAPIRouter);
 // catch 404 and forward to error handler
---
+++ api/routes/recipeAPI.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var appCall = express.Router();
+var pool = require('../db');
+
+const axios = require('axios');
+
+appCall.get("/search", async (req, res) => {
+  try {
+    // Extract the query parameter from the request
+    const query = req.query.q;
+
+    let apiUrl;
+
+    // Determine if the query is a name or a first letter
+    if (query.length === 1) {
+      // Search by first letter
+      apiUrl = `https://www.themealdb.com/api/json/v1/1/search.php?f=${query}`;
+    } else {
+      // Search by name
+      apiUrl = `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`;
+    }
+
+    // Fetch data from the API
+    const response = await axios.get(apiUrl);
+
+    // Return relevant results from the query
+    res.json(response.data.meals);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server Error");
+  }
+});
