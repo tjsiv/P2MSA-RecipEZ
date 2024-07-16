ff38c64553b8086304daeda782d9902bb6df75e0
JesusMLares <twinlares2@hotmail.com>
2024-07-15 22:14:32 -0700

    Styling changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='sticky top-0 z-50 flex justify-between items-center py-4'>
+    <div className='sticky top-0 z-50 flex justify-between items-center py-4 bg-black'>
         <h1 className='uppercase font-bold pl-4 text-4xl text-pink-200'>Recip<span className='text-blue-200'>EZ</span></h1>
         <ul className="flex">
             <li className='text-pink-200 pr-4 text-2xl'>Search</li>
--- client/src/components/RecipeCard.jsx
+++ client/src/components/RecipeCard.jsx
@@ -5,12 +5,12 @@ const RecipeCard = ({ results }) => {
     console.log(results),
     (
       <div>
-        <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8 ">
-          <h2 class="text-2xl font-bold tracking-tight">Recipes:</h2>
-          <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+        <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8 ">
+          <h2 className="text-2xl font-bold tracking-tight">Recipes:</h2>
+          <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
           {results.map((result, index) => (
-              <div key={index} class="group relative">
-                <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+              <div key={index} className="group relative">
+                <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
                   {/* Image */}
                   <img
                     src={result.strMealThumb}
@@ -19,19 +19,19 @@ const RecipeCard = ({ results }) => {
                   />
                 </div>
                 {/* Text Container */}
-                <div class="mt-4 flex justify-between">
+                <div className="mt-4 flex justify-between md:text-left">
                   <div>
-                    <h3 class="text-2xl">
-                      <a href="#">
+                    <h3 className="text-2xl">
+                      <a href="#" className="ml-4">
                         <span
                           aria-hidden="true"
-                          class="absolute inset-0"
+                          class="absolute inset-0 bg-gray-600 opacity-10 z-[-1] rounded-md"
                         ></span>
                         {/* Food Name */}
                         {result.strMeal}
                       </a>
                     </h3>
-                    <p class="mt-1 text-sm text-gray-500">
+                    <p className="mt-1 text-sm text-white m-4 md:text-left">
                       {/* Ingredients */}
                       {result.strIngredient1}, {result.strIngredient2}, {result.strIngredient3}, {result.strIngredient4}, {result.strIngredient5}, {result.strIngredient6}, {result.strIngredient7}, {result.strIngredient8}, {result.strIngredient9}, {result.strIngredient10}
                     </p>
