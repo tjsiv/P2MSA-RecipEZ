b055eb7af6dcff3c2c2cf3fde9072c0bf4dbea56
“Foxshane997” <Foxshane997@gmail.com>
2024-07-22 22:16:44 -0700

    changed logout button to red

--- client/src/components/UserProfile.jsx
+++ client/src/components/UserProfile.jsx
@@ -3,30 +3,31 @@ import React from "react";
 const UserProfile = ({ user, onLogout }) => {
   const profileStyles = {
     container: {
-      maxWidth: '600px',
-      margin: '0 auto',
-      padding: '20px',
-      border: '1px solid #ddd',
-      borderRadius: '8px',
-      boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-      textAlign: 'center'
+      maxWidth: "600px",
+      margin: "50px auto 0", // Adds margin to the top to push it down below the navbar
+      padding: "50px",
+      border: "1px solid #ddd",
+      borderRadius: "8px",
+      boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+      textAlign: "center",  
     },
     header: {
-      fontSize: '24px',
-      marginBottom: '10px',
+      fontSize: "24px",
+      marginBottom: "10px",
     },
     details: {
-      marginBottom: '20px',
+      marginBottom: "20px",
     },
     button: {
-      padding: '10px 20px',
-      fontSize: '16px',
-      color: '#fff',
-      backgroundColor: '#007bff',
-      border: 'none',
-      borderRadius: '5px',
-      cursor: 'pointer',
-    }
+      padding: "10px 20px",
+      fontSize: "16px",
+      backgroundColor: "red",
+      fontWeight: "bold",
+      color:"white",
+      border: "none",
+      borderRadius: "5px",
+      cursor: "pointer",  
+    },
   };
 
   return (
@@ -34,7 +35,9 @@ const UserProfile = ({ user, onLogout }) => {
       <h2 style={profileStyles.header}>Welcome, {user.username}!</h2>
       <p style={profileStyles.details}>Email: {user.email}</p>
       {/* Add more user profile details as needed */}
-      <button style={profileStyles.button} onClick={onLogout}>Logout</button>
+      <button className="button-logout" style={profileStyles.button} onClick={onLogout}>
+        Logout
+      </button>
     </div>
   );
 };
