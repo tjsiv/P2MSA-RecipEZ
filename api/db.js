176207af2680b4b9eac394ab7b51a219d36b307d
JesusMLares <twinlares2@hotmail.com>
2024-07-27 14:10:07 -0700

    Testing railway connection

--- api/db.js
+++ api/db.js
@@ -2,13 +2,36 @@
 require('dotenv').config()
 const Pool = require("pg").Pool
 
-// create a new pool instance with the given configuration
+// Create a new pool instance using DATABASE_URL
 const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME
-})
+    connectionString: process.env.DATABASE_URL, // Use DATABASE_URL provided by Railway
+    ssl: {
+        rejectUnauthorized: false // Necessary for external connections in some cases
+    }
+});
+
+// const poolConfig = {
+//     max: 5,
+//     min: 2,
+//     idleTimeoutMillis: 60000,
+// }
+
+// const DataBase = process.env.DB_NAMEl
+// const UserName = process.env.DB_USER;
+// const Password = process.env.DB_PASSWORD;
+// const Host = process.env.DB_HOST;
+// const Port = process.env.DB_PORT;
+
+// poolConfig.database = `postgres://${UserName}:${Password}@${Host}:${Port}/${DataBase}`
+
+
+// // create a new pool instance with the given configuration
+// const pool = new Pool({
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     database: process.env.DB_NAME
+// })
 
 module.exports = pool;
