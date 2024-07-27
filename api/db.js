b9516ccbffa526cb27a19b30b5d53195d21b9ef6
JesusMLares <twinlares2@hotmail.com>
2024-07-27 16:42:40 -0700

    Attempting to fix connection to railway

--- api/db.js
+++ api/db.js
@@ -1,6 +1,5 @@
-//require pool for performance
-require('dotenv').config()
-const Pool = require("pg").Pool
+require('dotenv').config();
+const Pool = require("pg").Pool;
 
 // Create a new pool instance using DATABASE_URL
 const pool = new Pool({
@@ -9,29 +8,5 @@ const pool = new Pool({
         rejectUnauthorized: false // Necessary for external connections in some cases
     }
 });
-
-// const poolConfig = {
-//     max: 5,
-//     min: 2,
-//     idleTimeoutMillis: 60000,
-// }
-
-// const DataBase = process.env.DB_NAMEl
-// const UserName = process.env.DB_USER;
-// const Password = process.env.DB_PASSWORD;
-// const Host = process.env.DB_HOST;
-// const Port = process.env.DB_PORT;
-
-// poolConfig.database = `postgres://${UserName}:${Password}@${Host}:${Port}/${DataBase}`
-
-
-// // create a new pool instance with the given configuration
-// const pool = new Pool({
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT,
-//     database: process.env.DB_NAME
-// })
 
 module.exports = pool;
